51251cb32ef4213fa49899214d1bb280c314e94e
ozan <ozanakaydin@gmail.com>
2024-06-14 18:06:33 +0300

    Index page updated

--- src/CoderMind.WebApp/Pages/Index.cshtml.cs
+++ src/CoderMind.WebApp/Pages/Index.cshtml.cs
@@ -1,20 +1,52 @@
+using CoderMind.Application.Services.Interfaces;
+using CoderMind.Shared.Dtos.UserDtos;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace CoderMind.WebApp.Pages
 {
-    public class IndexModel : PageModel
+    public class IndexModel(IUserService userService) : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
 
-        public IndexModel(ILogger<IndexModel> logger)
-        {
-            _logger = logger;
-        }
+        [BindProperty]
+        public RegisterDto RegisterDto { get; set; }
+
+        [BindProperty]
+        public LoginDto LoginDto { get; set; }
 
         public void OnGet()
         {
 
+        }
+
+        public async Task<IActionResult>OnPostRegister()
+        {
+            var result = await userService.Register(RegisterDto);
+            if (!result.IsSuccess)
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error);
+                }
+                return Page();
+            }
+
+            return RedirectToPage("/Index");
+        }
+
+        public async Task<IActionResult> OnPostLogin()
+        {
+            var result = await userService.Login(LoginDto);
+            if (!result.IsSuccess)
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error);
+                }
+                return Page();
+            }
+
+            return RedirectToPage("/Index");
         }
     }
 }
