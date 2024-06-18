4e1229b0e1c17c03bd4652e3320b1de2131a58b1
ozan <ozanakaydin@gmail.com>
2024-06-18 10:33:41 +0300

    Create subject page created

---
+++ src/CoderMind.WebApp/Pages/Subject/Create.cshtml.cs
@@ -0,0 +1,25 @@
+using CoderMind.Application.Services.Interfaces;
+using CoderMind.Shared.Dtos.SubjectDtos;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CoderMind.WebApp.Pages.Subject;
+
+public class CreateModel(ISubjectService subjectService, ITechnologyService technologyService) : PageModel
+{
+    [BindProperty]
+    public CreateSubjectDto CreateSubject { get; set; } = new();
+
+    public async Task OnGet(string technologyId)
+    {
+        CreateSubject.TechnologyId = technologyId;
+        var tehnology = await technologyService.GetTechnologyAsync(technologyId);
+        ViewData["TechnologyName"] = tehnology.Name;
+    }
+
+    public async Task<IActionResult> OnPostCreateSubject(CancellationToken cancellationToken)
+    {
+        await subjectService.CreateSubjectAsync(CreateSubject, cancellationToken);
+        return RedirectToPage("/Index");
+    }
+}
