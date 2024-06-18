6e4860c388cf497c9d8e276e4f822aeaf391396f
ozan <ozanakaydin@gmail.com>
2024-06-18 10:41:15 +0300

    Subject detail page created

---
+++ src/CoderMind.WebApp/Pages/Subject/Detail.cshtml.cs
@@ -0,0 +1,37 @@
+using CoderMind.Application.Services.Interfaces;
+using CoderMind.Shared.Dtos.ContentDtos;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CoderMind.WebApp.Pages.Subject;
+
+public class DetailModel(IContentService contentService, ISubjectService subjectService) : PageModel
+{
+    [BindProperty]
+    public GetContentDto? Content { get; set; }
+
+    [BindProperty]
+    public CreateContentDto CreateContent { get; set; } = new();
+
+    [BindProperty]
+    public string SubjectId { get; set; }
+
+    public async Task OnGetAsync(string id)
+    {
+        CreateContent.SubjectId = id;
+        SubjectId = id;
+        Content = await contentService.GetSubjectContentBySubjectIdAsync(id);
+    }
+
+    public async Task<IActionResult> OnPostCreateContent()
+    {
+        await contentService.CreateContentAsync(CreateContent);
+        return RedirectToPage("/Index");
+    }
+
+    public async Task<IActionResult> OnPostDeleteSubject(string subjectId)
+    {
+        await subjectService.DeleteSubjectAsync(subjectId);
+        return RedirectToPage("/Index");
+    }
+}
