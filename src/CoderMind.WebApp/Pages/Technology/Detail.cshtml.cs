943bee5b395e46bd7becf449ade99b37fe4eeac5
ozan <ozanakaydin@gmail.com>
2024-06-18 10:24:59 +0300

    Technology detail page created

---
+++ src/CoderMind.WebApp/Pages/Technology/Detail.cshtml.cs
@@ -0,0 +1,23 @@
+using CoderMind.Application.Services.Interfaces;
+using CoderMind.Shared.Dtos.TechnologyDtos;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CoderMind.WebApp.Pages.Technology;
+
+public class DetailModel(ITechnologyService technologyService) : PageModel
+{
+    [BindProperty]
+    public GetTechnologyDto TechnologyDto { get; set; }
+
+    public async Task OnGet(string technologyId)
+    {
+        TechnologyDto = await technologyService.GetTechnologyAsync(technologyId);
+    }
+
+    public async Task<IActionResult> OnPostDeleteTechnology(string technologyId)
+    {
+        await technologyService.DeleteTechnologyAsync(technologyId);
+        return RedirectToPage("/Index");
+    }
+}
